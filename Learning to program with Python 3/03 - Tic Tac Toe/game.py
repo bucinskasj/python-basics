2bd116ff1f9f40120277ca760867683761fd1eee
Jonas Bucinskas <jonas@bucinskas.io>
2023-11-14 16:10:07 +0200

    Tic tac toe game

--- Learning to program with Python 3/03 - Tic Tac Toe/game.py
+++ Learning to program with Python 3/03 - Tic Tac Toe/game.py
@@ -1,17 +1,104 @@
-game = [[0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]]
+import itertools
 
-def game_board(player=0, row=0, column=0, just_display=False):
-  print('   a  b  c')
-  if not just_display:
-    game[row][column] = player
-  for count, row in enumerate(game):
-    print(count, row)
+def win(current_game):
+
+  def all_same(l):
+    if l.count(l[0]) == len(l) and l[0] != 0:
+      return True
+    else:
+      return False
+    
+  # Horizontal winner
+  for row in game:
+    print(row)
+    if all_same(row):
+      print(f"Player {row[0]} is the winner horizontally")
+      return True
+    
+  # Diagonal
+  diags = []
+  for col, row in enumerate(reversed(range(len(game)))):
+    diags.append(game[row][col])
+  if all_same(diags):
+    print(f"Player {diags[0]} is the winner diagonally (/)")
+    return True
+
+  diags = []
+  for ix in range(len(game)):
+    diags.append(game[ix][ix])
+  if all_same(diags):
+    print(f"Player {diags[0]} is the winner diagonally (\\)")
+    return True
+  
+  # Vertical
+
+  for col in range(len(game)):
+    check = []
+  
+    for row in game:
+      check.append(row[col])
+
+    if all_same(check):
+      print(f"Player {check[0]} is the winner verticallty (|)")
+      return True
+  
+  return False
 
 
-game_board(just_display=True)
-game_board(player=1, row=2, column=1)
+def game_board(game_map, player=0, row=0, column=0, just_display=False):
+  try:
+    if game_map[row][column] != 0:
+      print("This position is occupied. Choose another!")
+      return game_map, False
+    print('   a  b  c')
+    if not just_display:
+      game_map[row][column] = player
+    for count, row in enumerate(game_map):
+      print(count, row) 
+    return game_map, True
+  except IndexError as e:
+    print('Error: make sure you input row/colummn as 0 1 or 2?', e)
+    return game_map, False
+  except Exception as e:
+    print('Something went wrong!', e)
+    return game_map, False
 
+play = True
+players = [1, 2]
+while play:
+  game = [[0, 0, 0],
+          [0, 0, 0],
+          [0, 0, 0]]
+  game_won = False
+  game, _ = game_board(game, just_display=True)
+  player_choice = itertools.cycle([1, 2])
+  while not game_won:
+    current_player = next(player_choice)
+    print(f"Current player {current_player}")
+    played = False
+
+    while not played:
+      column_choice = int(input('What column do you want to play? (0, 1, 2): '))
+      row_choice = int(input('What row do you want to play? (0, 1, 2): '))
+      game, played = game_board(game, current_player, row_choice, column_choice)
+    if win(game):
+      game_won = True
+      again = input('The game is over. Would you like to play again? (y/n) ')
+      if again.lower() == 'y':
+        print('Restarting...')
+      elif again.lower() == 'n':
+        print('Byeeeee')
+        play = False
+      else:
+        print('Not a valid answer, so... c u later aligator')
+        play = False
+
+
+
+
+# cols = list(reversed(range(len(game))))
+# rows = range(len(game))
+
+# win(game)
 
 
--- Learning to program with Python 3/Readme.md
+++ Learning to program with Python 3/Readme.md
@@ -4,3 +4,4 @@ Free course on Youtube https://www.youtube.com/watch?v=eXBD2bB9-RA&list=PLQVvvaa
 
 - Python built in functions
 - Linters and syntax rules for Python
+- Learn how else/finally/raise works in try except
